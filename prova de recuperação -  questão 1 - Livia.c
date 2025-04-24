19ab92bbcb12e2b3000bd4eeee72942af24cf8e4
livia <liviamajzoubsilva19@gmail.com>
2025-04-24 16:17:24 -0300

    Add files via upload

---
+++ prova de recuperação -  questão 1 - Livia.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+       float altura, largura, area;
+       int litro;
+
+    printf("Digite a altura(em metros): ");
+    scanf("%f", &altura);
+
+    printf("Digite a largura(em metros): ");
+    scanf("%f", &largura);
+
+       area = (altura * largura);
+
+       litro = ceil(area/2);
+
+
+    printf("A area da parede eh: %f", area);
+
+    printf("A quantidade de tinta eh: %d", litro);
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
